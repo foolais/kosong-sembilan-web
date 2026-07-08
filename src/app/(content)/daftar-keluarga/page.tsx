9358d82738e3577fb08d126a520e8ed141aa50e3
Foolais <wahyu.esya17@gmail.com>
2026-07-08 21:31:10 +0700

    fix: add suspense when loading id

--- src/app/(content)/daftar-keluarga/page.tsx
+++ src/app/(content)/daftar-keluarga/page.tsx
@@ -1,15 +1,21 @@
 import FamilyTabs from "@/components/family-tabs";
 import ListFamily from "@/components/list-family";
 import SearchInput from "@/components/search-input";
+import { Loader } from "lucide-react";
+import { Suspense } from "react";
 
 const DaftarPage = () => {
   return (
     <div className="mx-auto space-y-4">
       <h2 className="text-lg font-semibold">Daftar Keluarga</h2>
-      <div className="mb-6">
-        <FamilyTabs />
-      </div>
-      <SearchInput />
+      <Suspense fallback={<Loader className="animate-spin size-8" />}>
+        <div className="mb-6">
+          <FamilyTabs />
+        </div>
+      </Suspense>
+      <Suspense fallback={<Loader className="animate-spin size-8" />}>
+        <SearchInput />
+      </Suspense>
       <ListFamily />
     </div>
   );
--- src/app/(content)/dashboard/page.tsx
+++ src/app/(content)/dashboard/page.tsx
@@ -1,12 +1,16 @@
 import AnalyticCard from "@/components/analytic-card";
 import SearchInput from "@/components/search-input";
 import WelcomingText from "@/components/welcoming-text";
+import { Loader } from "lucide-react";
+import { Suspense } from "react";
 
 const DashboardPage = () => {
   return (
     <div className="space-y-4">
       <WelcomingText />
-      <SearchInput />
+      <Suspense fallback={<Loader className="animate-spin size-8" />}>
+        <SearchInput />
+      </Suspense>
       <div className="space-y-2">
         <h3 className="font-semibold">Statistik</h3>
         <AnalyticCard />
